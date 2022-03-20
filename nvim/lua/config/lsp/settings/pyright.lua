3b6cadf808af7a4e34167c07ae9ab4dc7225131f
Leonam Silva <leonam.silva.cc@gmail.com>
2022-03-19 21:04:48 -0300

    Some lsp tries to improve the vevn searching, but i decided to mantain old approach and creating a pyright.json in project root directory

--- nvim/lua/config/lsp/settings/pyright.lua
+++ nvim/lua/config/lsp/settings/pyright.lua
@@ -1,7 +1,34 @@
+-- local found_util, util = pcall(require, "lspconfig.util")
+-- if not found_util then
+-- 	vim.notify("Could not find lspconfig.util in pyright.lua")
+-- 	return
+-- end
+
+-- local function get_python_path(workspace)
+--     if vim.env.VIRTUAL_ENV then
+--             return util.path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+--     else
+--         if util.path.exists(util.path.join(workspace, "poetry.lock")) then
+--             local venv_ok, venv = pcall(vim.fn.system, "cd " .. workspace .. " && poetry env info -p")
+--             if venv_ok then
+--                 return util.path.join(vim.fn.trim(venv), "bin", "python")
+--             end
+--         end
+--     end
+--     return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+-- end
+
 return {
-  settings = {
-    python = {
-        venvPath = '/home/leonam/.pyenv/versions/',
+    settings = {
+        python = {
+            analysis = {
+                typeCheckingMode = "strict",
+            },
+            venvPath = '/home/leonam/.pyenv/versions/'
+        },
     },
-  },
+    -- on_init = function(client)
+    --     local ppath = get_python_path(client.config.root_dir)
+    --     client.config.settings.python.pythonPath = ppath
+    -- end,
 }
