cb2ae2eeb9afabfc721e42129a73769652e47c89
Leonam Silva <leonam.silva.cc@gmail.com>
2022-03-29 18:21:21 -0300

    Passando para o telescope o lsp code actions e os objetos dentro do workspace

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -14,8 +14,10 @@ local mappings = {
   -- Git Status
   {"n", "<Leader>gs", [[<Cmd>Telescope git_status<CR>]], opts},
   {"n", "<Leader>gb", [[<Cmd>Telescope git_branches<CR>]], opts},
-  -- tags 
-  {"n", "<Leader>t", [[<Cmd>Telescope tags<CR>]], opts},
+  -- lsp symbols
+  {"n", "<Leader>t", [[<Cmd>Telescope lsp_dynamic_workspace_symbols<CR>]], opts},
+  -- lsp  code actions
+  {"n", "<space>ca", [[<Cmd>Telescope lsp_code_actions<CR>]], opts}
 }
 for _, val in pairs(mappings) do
   vim.api.nvim_set_keymap(unpack(val))
