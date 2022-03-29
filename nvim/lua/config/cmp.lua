cf28e2dc32ce149670c87e0ea76c48ba9b0f878d
Leonam Silva <leonam.silva.cc@gmail.com>
2022-03-29 18:19:49 -0300

    Adicioanndo plugin de snippets ao nvim-cmp

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -3,19 +3,22 @@
 local cmp = require("cmp")
 local compare = require("cmp.config.compare")
 local lspkind = require('lspkind')
+local cmp_ultisnips_mappings = require("cmp_nvim_ultisnips.mappings")
 
 cmp.setup({
   snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
+        expand = function(args)
+          vim.fn["UltiSnips#Anon"](args.body)
+        end,
   },
   sources = {
-      { name = "treesitter"},
+      { name = "ultisnips" },
+      { name = "treesitter" },
       { name = "nvim_lsp" },
+      { name = "tags" },
+      { name = 'look', keyword_length = 2, option = {convert_case = true, loud = true} },
       { name = "buffer" },
       { name = "path" },
-      { name = "vsnip" },
       { name = "nvim_lua" },
   },
   mapping = {
@@ -38,6 +41,18 @@ cmp.setup({
         fallback()
       end
     end,
+     ["<Tab>"] = cmp.mapping(
+          function(fallback)
+            cmp_ultisnips_mappings.expand_or_jump_forwards(fallback)
+          end,
+          { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+        ),
+    ["<S-Tab>"] = cmp.mapping(
+          function(fallback)
+            cmp_ultisnips_mappings.jump_backwards(fallback)
+          end,
+          { "i", "s", --[[ "c" (to enable the mapping in command mode) ]] }
+        )
   },
   sorting = {
     comparators = {
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -127,12 +127,10 @@ return require("packer").startup(function(use)
        "hrsh7th/cmp-nvim-lsp",
        "hrsh7th/cmp-nvim-lua",
        "hrsh7th/cmp-path",
-       "hrsh7th/cmp-vsnip",
-       "octaltree/cmp-look",
+       "hrsh7th/cmp-cmdline",
        "quangnguyen30192/cmp-nvim-tags",
-       "quangnguyen30192/cmp-nvim-ultisnips",
        "ray-x/cmp-treesitter",
-       "saadparwaiz1/cmp_luasnip",
+       "quangnguyen30192/cmp-nvim-ultisnips",
        "onsails/lspkind-nvim",
      }}
      -- lsp, completion, linting and snippets
