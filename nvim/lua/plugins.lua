a64ea30e3fe36500aa3dbb769cb513d4a89d69d7
Leonam Silva <leonam.silva.cc@gmail.com>
2022-04-09 18:11:53 -0300

    Neotree, novo plugin para visualização de arquivos

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -69,6 +69,18 @@ return require("packer").startup(function(use)
     },
     config = function() require('config.nvim-tree') end
   }
+
+  use {
+      "nvim-neo-tree/neo-tree.nvim",
+       branch = "v2.x",
+       requires = {
+         "nvim-lua/plenary.nvim",
+         "kyazdani42/nvim-web-devicons",
+         "MunifTanjim/nui.nvim"
+       },
+       config = function() require("config.neo-tree") end
+  }
+
   use {
     "gelguy/wilder.nvim",
     run = ":UpdateRemotePlugins",
