f518b1077a6b7c1e7ecf2e954a102d4b581bb11b
Leonam Silva <leonam.silva.cc@gmail.com>
2022-04-09 18:16:08 -0300

    Remoção do direnv (ha muito nao utilizado) Alias ficam no começo do arquivo

--- my_prompt.sh
+++ my_prompt.sh
@@ -1,3 +1,9 @@
+# Alias
+alias k=kubectl
+alias v=nvim-0.6
+alias g=git
+alias tor="/opt/tor-browser_en-US/Browser/start-tor-browser -detach"
+
 # pyenv
  export PATH="/home/leonam/.pyenv/bin:$PATH"
  eval "$(pyenv init -)"
@@ -6,17 +12,8 @@
 # Python Local
 export PATH="$HOME/.poetry/bin/:$HOME/.local/bin/:$PATH"
 
-# DirEnv
-# eval "$(direnv hook bash)"
-
 # Fzf Finder
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
-
-# Alias
-alias k=kubectl
-alias v=nvim-0.6
-alias g=git
-alias tor="/opt/tor-browser_en-US/Browser/start-tor-browser -detach"
 
 # Evitar que o vim trave no Ctrl-S
 stty -ixon
