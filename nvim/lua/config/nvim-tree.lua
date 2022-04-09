716ddf578aaee188c535228b94c40a24efc26c5a
Leonam Silva <leonam.silva.cc@gmail.com>
2022-04-09 18:15:18 -0300

    Retirando o f9 do nvim-tree

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -1,4 +1,13 @@
-require'nvim-tree'.setup()
+require'nvim-tree'.setup({
+    view = {
+        relativenumber = true,
+        number = true,
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+    }
+})
 
-local keymap = vim.api.nvim_set_keymap
-keymap( "n", "<F9>", ":NvimTreeToggle<CR>", { noremap = true })
+-- local keymap = vim.api.nvim_set_keymap
+-- keymap( "n", "<F9>", ":NvimTreeToggle<CR>", { noremap = true })
