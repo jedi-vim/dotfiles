ff11b614d6697153c67ddd3b6dffdad4e2f12a51
Leonam Silva <leonam.silva.cc@gmail.com>
2022-04-26 01:45:43 -0300

    Pequenos ajustes na conf do Lsp

--- nvim/lua/config/lsp/init.lua
+++ nvim/lua/config/lsp/init.lua
@@ -21,24 +21,19 @@ for _, svr in pairs(required_servers) do
   end
 end
 
-local function make_config()
-    return {
-        on_attach = require('config.lsp.handlers').on_attach,
-        capabilities = require('config.lsp.handlers').capabilities,
-        handlers = {
-            ["textDocument/publishDiagnostics"] = vim.lsp.with( vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }),
-        }
+local opts = {
+    on_attach = require('config.lsp.handlers').on_attach,
+    capabilities = require('config.lsp.handlers').capabilities,
+    handlers = {
+        ["textDocument/publishDiagnostics"] = vim.lsp.with( vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false }),
     }
-end
+}
 
-local lsp_installer = require("nvim-lsp-installer")
-lsp_installer.on_server_ready(function(server)
-    local opts = make_config()
 
+require("nvim-lsp-installer").on_server_ready(function(server)
     -- if server.name == "pyright" then
     --     local pyright_opts = require("config.lsp.settings.pyright")
     --         opts = vim.tbl_deep_extend("force", pyright_opts, opts)
     -- end
-
     server:setup(opts)
 end)
