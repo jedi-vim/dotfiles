17757de819cbf04fd55217c04b0c1605a1d4368d
Leonam Silva <leonam.silva.cc@gmail.com>
2022-05-22 21:38:37 -0300

    null-ls simplified conf

---
+++ nvim/lua/config/lsp/null-ls.lua
@@ -0,0 +1,46 @@
+local M = {}
+
+local nls = require "null-ls"
+local nls_utils = require "null-ls.utils"
+
+local formatting = nls.builtins.formatting
+local diagnostics = nls.builtins.diagnostics
+local actions = nls.builtins.code_actions
+
+local sources = {
+  -- formatting
+  formatting.prettierd,
+  formatting.shfmt,
+  formatting.fixjson,
+  formatting.black,
+  -- formatting.autopep8.with({ extra_args = { "--in-place", "-a", "-a" } }),
+  formatting.isort,
+
+  -- diagnostics
+  diagnostics.write_good,
+  diagnostics.eslint_d,
+  -- diagnostics.flake8,
+  diagnostics.credo,
+  diagnostics.zsh,
+
+  -- code actions
+  actions.gitsigns,
+  actions.gitrebase,
+  -- actions.refactoring
+
+  -- hover
+  -- b.hover.dictionary,
+}
+
+function M.setup(opts)
+  nls.setup {
+    -- debug = true,
+    debounce = 150,
+    save_after_format = false,
+    sources = sources,
+    on_attach = opts.on_attach,
+    root_dir = nls_utils.root_pattern ".git",
+  }
+end
+
+return M
