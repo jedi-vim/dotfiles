e47416de4f1b0635778eb6cb3f4ce1dfff8f77f9
Leonam Silva <leonam.silva.cc@gmail.com>
2023-05-20 13:18:21 -0300

    Outline panel on the left now

--- nvim/after/plugin/symbols_outline.lua
+++ nvim/after/plugin/symbols_outline.lua
@@ -1,21 +1,21 @@
 local ok, symbols_outline = pcall(require, "symbols-outline")
 if not ok then
-    return
+  return
 end
 symbols_outline.setup({
-    highlight_hovered_item = true,
-    show_guides = true,
-    auto_preview = false, -- experimental
-    position = 'right',
-    autoclose = true,
-    keymaps = {
-        close = "<Esc>",
-        goto_location = "<Cr>",
-        focus_location = "o",
-        hover_symbol = "<C-space>",
-        rename_symbol = "r",
-        code_actions = "a"
-    }
+  highlight_hovered_item = true,
+  show_guides = true,
+  auto_preview = false, -- experimental
+  position = 'left',
+  autoclose = true,
+  keymaps = {
+    close = "<Esc>",
+    goto_location = "<Cr>",
+    focus_location = "o",
+    hover_symbol = "<C-space>",
+    rename_symbol = "r",
+    code_actions = "a"
+  }
 })
 
 vim.keymap.set("n", "<F6>", "[[ <Cmd>SymbolsOutline<CR> ]]", {})
