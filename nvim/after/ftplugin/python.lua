b84a88e04046b0879a1a4c801abeeb7bcfe1adad
Leonam Silva <leonam.silva.cc@gmail.com>
2023-05-20 14:39:00 -0300

    enable colorcolumn and conf to tabshift

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,11 +1,11 @@
 local opt = vim.opt_local
--- opt.autoindent = true
--- opt.cindent = true
--- opt.cinwords = "if,elif,else,for,while,try,except,finally,def,class,with"
+opt.autoindent = true
+opt.cindent = true
+opt.cinwords = "if,elif,else,for,while,try,except,finally,def,class,with"
 opt.colorcolumn = "80"
 opt.copyindent = true
 opt.expandtab = true
-opt.formatoptions= "jntcoql"
+opt.formatoptions = "jntcoql"
 -- opt.smartindent = true
 opt.shiftwidth = 4
 opt.softtabstop = 4
