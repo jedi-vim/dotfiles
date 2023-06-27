0249c566a4c144affd5166cfe60772ca821cc3e9
Leonam Silva <leonam.silva.cc@gmail.com>
2023-06-27 14:36:26 -0300

    setup: adding some libs with brew, addingo powerlevel with git

--- .configure-env.sh
+++ .configure-env.sh
@@ -1,23 +1,41 @@
-MY_USER=leonam
+MY_USER=$(whoami)
 
-apt-get install \
-    build-essential \
+# Install general deps
+apt-get update
+apt-get install build-essential
     git \
     curl \
     autojump \
     nodejs \
-    zsh \
-    fonts-powerline
+    fonts-powerline \
+    libxml2-dev \
+    libxslt-dev \
+    python-dev \
+    libmemcached-dev \
+    zlib1g-dev
+
+# Kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 
 # Oh-my-Zsh
 chsh -s $(which zsh) $MY_USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# NerdFonts
-# Ag Silver Search
+# PowerLevel10K Zsh Theme 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Neovim
 NEOVIM_VERSION="0.8.2"
 curl -LO https://github.com/neovim/neovim/releases/download/v$NEOVIM_VERSION/nvim.appimage
 mv nvim.appimage /home/$USER/.local/bin/nvim
 chmod u+x /home/$USER/.local/bin/nvim
+ln -s $USER/.nvim-$NEOVIM_VERSION/bin/nvim $USER/.local/bin/nvim-$NEOVIM_VERSION
+
+# Install HomeBrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install \
+    gnu-sed ripgrep the_silver_searcher \
+    pyenv
+
+# Symbolic links of files that must have been in ~
+ln -s $HOME/.config/zshrc $HOME/.zshrc
+
