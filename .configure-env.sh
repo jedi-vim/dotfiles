bb00c4a400e8baea60ad0d0c8e9e7b893cdd749b
Leonam Silva <leonam.silva.cc@gmail.com>
2023-08-06 15:19:41 -0300

    install NVM

--- .configure-env.sh
+++ .configure-env.sh
@@ -20,8 +20,12 @@ curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 # Oh-my-Zsh
 chsh -s $(which zsh) $MY_USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 # PowerLevel10K Zsh Theme 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# NVM 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 # Neovim
 NEOVIM_VERSION="0.8.2"
