e30ef7cb73b4e3797e8447da5aab21ef6213113a
Leonam Silva <leonam.silva.cc@gmail.com>
2023-11-18 20:26:38 -0300

    install: Instalar docker e golang, remover deps para execucao do neovim

--- .pre-configure-env.sh
+++ .pre-configure-env.sh
@@ -15,10 +15,22 @@ apt-get -y \
     htop \
     zsh \
     apt-transport-https \
-    libfuse2
+    ca-certificates \
+    software-properties-common
 
 # KubeCTL
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list \
 && apt-get update \
 && apt-get install -y kubectl
+
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && \
+    sudo apt update && \
+    apt-get install -y docker-ce
+# TODO: Depois de instalar preciso colocar meu usuario no grupo docker: usermod -aG docker ${USER}
+
+# Golang
+curl -s -L https://go.dev/dl/go1.21.4.linux-amd64.tar.gz | tar xzv -C /usr/local
+
