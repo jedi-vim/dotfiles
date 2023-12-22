3fee065f77691a98aa24c9e2760a03c274f37581
Leonam Silva <leonam.silva.cc@gmail.com>
2023-12-22 19:05:10 -0300

    install: Corrigir erros da instalacao
    Adicionar Nerd Font
    Adicionar .desktop do kitty

--- .configure-env.sh
+++ .configure-env.sh
@@ -3,6 +3,10 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k && \
 rm $HOME/.zshrc && ln -s $HOME/.config/zshrc $HOME/.zshrc
 
+echo "\n\n###### Nerd Font #############"
+wget -P $HOME/.local/share/fonts/ https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/Meslo/L/Regular/MesloLGLNerdFont-Regular.ttf
+wget -P $HOME/.local/share/fonts/ https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/ComicShannsMono/ComicShannsMonoNerdFont-Regular.otf
+
 echo "\n\n####### Install HomeBrew ######"
 git clone https://github.com/Homebrew/brew
 export PATH=$HOME/brew/bin:$PATH
@@ -10,6 +14,11 @@ brew install gnu-sed ripgrep the_silver_searcher pyenv
 
 echo "\n\n####### Kitty ################"
 curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+# Update the paths to the kitty and its icon in the kitty.desktop file(s)
+sed -i "s|Icon=kitty|Icon=$HOME/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+sed -i "s|Exec=kitty|Exec=$HOME/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 
 echo "\n\n######### Installing NVM $=#####"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
