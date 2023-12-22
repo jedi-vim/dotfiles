ebfed1577f7b1870566b622793e19e9d7e5b67a4
Leonam Silva <leonam.silva.cc@gmail.com>
2023-12-22 19:06:27 -0300

    installl: Corrigir erros da instalacao
    INstalar Docker compose
    Adicionar usuario ao grupo do docker com uso de dialog para fornecer o nome

--- .pre-configure-env.sh
+++ .pre-configure-env.sh
@@ -2,6 +2,7 @@
 apt-get update
 apt-get -y \
     install build-essential \
+    dialog \
     git \
     curl \
     autojump \
@@ -19,18 +20,31 @@ apt-get -y \
     software-properties-common
 
 # KubeCTL
+dialog --title "Aguarde" \
+    --infobox "Instalando Docker KubeCTL"
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list \
 && apt-get update \
 && apt-get install -y kubectl
 
 # Docker
+dialog --title "Aguarde" \
+    --infobox "Instalando Docker"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && \
     sudo apt update && \
     apt-get install -y docker-ce
-# TODO: Depois de instalar preciso colocar meu usuario no grupo docker: usermod -aG docker ${USER}
+USER_NAME=$(dialog --inputbox 'Usuario que vai usar o docker:' 0 0  2> /dev/null)
+usermod -aG docker ${USER_NAME}
+
+# Docker Compose
+dialog --title "Aguarde" \ 
+    --infobox "Instalando Docker-Compose"
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Golang
+dialog --title "Aguarde" \
+    --infobox "Instalando GoLang"
 curl -s -L https://go.dev/dl/go1.21.4.linux-amd64.tar.gz | tar xzv -C /usr/local
 
