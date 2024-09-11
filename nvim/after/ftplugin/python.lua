e58d646077f725e4ab38933d0d517d03d338b319
Leonam Silva <leonam.silva.cc@gmail.com>
2024-09-11 09:37:05 -0300

    neovim: python after file with new cworkds and comment func to add {} on f strings

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt_local
 opt.autoindent = true
 opt.cindent = true
-opt.cinwords = "if,elif,else,for,while,try,except,finally,def,class,with"
+opt.cinwords = "if,elif,else,for,while,try,except,finally,def,class,with,match,case,async"
 opt.colorcolumn = "80"
 opt.copyindent = true
 opt.expandtab = true
@@ -10,23 +10,34 @@ opt.shiftwidth = 4
 opt.softtabstop = 4
 opt.tabstop = 8
 
-vim.api.nvim_create_autocmd("InsertCharPre", {
-  pattern = { "*.py" },
-  group = vim.api.nvim_create_augroup("py-fstring", { clear = true }),
-  callback = function(params)
-    if vim.v.char ~= "{" then return end
+-- vim.api.nvim_create_autocmd("InsertCharPre", {
+--   pattern = { "*.py" },
+--   group = vim.api.nvim_create_augroup("py-fstring", { clear = true }),
+--   callback = function(params)
 
-    local node = vim.treesitter.get_node()
+--     if vim.v.char ~= "{" then
+--       return
+--     end
 
-    if not node then return end
+--     local node = vim.treesitter.get_node_at_cursor()
+--     if not node then
+--       return
+--     end
 
-    if node:type() ~= "string" then node = node:parent() end
+--     if node:type() ~= "string" then
+--       node = node:parent()
+--     end
 
-    if not node or node:type() ~= "string" then return end
-    local row, col, _, _ = vim.treesitter.get_node_range(node)
-    local first_char = vim.api.nvim_buf_get_text(params.buf, row, col, row, col + 1, {})[1]
-    if first_char == "f" then return end
+--     if not node or node:type() ~= "string" then
+--       return
+--     end
 
-    vim.api.nvim_input("<Esc>m'" .. row + 1 .. "gg" .. col + 1 .. "|if<esc>`'la")
-  end,
-})
+--     local row, col, _, _ = vim.treesitter.get_node_range(node)
+--     local first_char = vim.api.nvim_buf_get_text(params.buf, row, col, row, col + 1, {})[1]
+--     if first_char == "f" then
+--       return
+--     end
+
+--     vim.api.nvim_input("<Esc>m'" .. row + 1 .. "gg" .. col + 1 .. "|if<esc>`'la")
+--   end,
+-- })
