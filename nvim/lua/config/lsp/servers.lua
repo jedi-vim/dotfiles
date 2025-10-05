1354fc79caaadd5d73483a05dd5a395791858529
Leonam Silva <leonam.silva.cc@gmail.com>
2025-10-05 19:20:36 -0300

    fix: corrigindo chamada a setup do mason lsp config

--- nvim/lua/config/lsp/servers.lua
+++ nvim/lua/config/lsp/servers.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 M.setup = function()
-  require("mason").setup {}
+  local mason_ok, mason = pcall(require, "mason")
+  if not mason_ok then
+    return
+  end
+  mason.setup {}
   require("mason-lspconfig").setup {
     ensure_installed = {
       "lua_ls",      -- lua
@@ -12,15 +16,15 @@ M.setup = function()
       "yamlls",      -- yaml
       "vimls",       -- vim
       "jsonls",      -- json
+    },
+    handlers = {
+      function(server_name)
+        if server_name then
+          require("lspconfig")[server_name].setup({})
+        end
+      end
     }
   }
-  require("mason-lspconfig").setup_handlers({
-    function(server_name)
-      if server_name then
-        require("lspconfig")[server_name].setup({})
-      end
-    end
-  })
 end
 
 return M
